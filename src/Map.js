e3ada0a8ff21def8782b4bfc8a5d1f4a85a3d0e3
Struan-Wall <struan03@hotmail.com>
2020-07-08 18:51:22 +1200

    refactor react component and display google maps

--- src/Map.js
+++ src/Map.js
@@ -1,22 +1,52 @@
-import React from 'react';
+//import React from 'react';
+import React, { Component } from "react"
 import './App.css';
-import { GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 
-function Map() {
+export class MapContainer extends Component {
+  constructor(props) {
+    super(props);
 
-    render() {
-        return (
-            <Map
-              google={this.props.google}
-              zoom={8}
-              style={mapStyles}
-              initialCenter={{ lat: 47.444, lng: -122.176}}
-            />
-        );
-      }
+    this.state = {
+      stores: [{lat: 47.49855629475769, lng: -122.14184416996333},
+              {latitude: 47.359423, longitude: -122.021071},
+              {latitude: 47.2052192687988, longitude: -121.988426208496},
+              {latitude: 47.6307081, longitude: -122.1434325},
+              {latitude: 47.3084488, longitude: -122.2140121},
+              {latitude: 47.5524695, longitude: -122.0425407}]
+    }
+  }
 
+  displayMarkers = () => {
+    return this.state.stores.map((store, index) => {
+      return <Marker key={index} id={index} position={{
+       lat: store.latitude,
+       lng: store.longitude
+     }}
+     onClick={() => console.log("You clicked me!")} />
+    })
+  }
+
+  render() {
+    return (
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176}}
+        >
+          {this.displayMarkers()}
+        </Map>
+    );
+  }
+}
       export default GoogleApiWrapper({
-        apiKey: AIzaSyCJcJoGDA0X2PMtfefER6hBCRPx8HW83RA
+        apiKey: 'AIzaSyCJcJoGDA0X2PMtfefER6hBCRPx8HW83RA'
       })(MapContainer);
-      
+
+
+      const mapStyles = {
+        width: '100%',
+        height: '100%',
+      };
